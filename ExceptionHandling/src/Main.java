6a6f370cc2953a7cce6c599398a40899bcfc2af8
Yusuf Ahmet Tokgoz <ahmetyusuftokgoz@gmail.com>
2023-10-26 20:42:23 +0300

    Exception Handling

---
+++ ExceptionHandling/src/Main.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		try
+		{
+			int[] sayilar = new int[] {1,2,3};
+			System.out.println(sayilar[5]);
+		}catch (ArrayIndexOutOfBoundsException exception) {
+			System.out.println(exception);
+		}catch (StringIndexOutOfBoundsException exception) {
+			
+		}catch(Exception exception) {
+			System.out.println("Loglandı" + exception);
+		}
+		finally {
+			System.out.println("Ben her zaman çalışırım.");
+		}
+			
+
+	}
+
+}
